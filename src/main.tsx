c8cfed5835142ab7b56e9729687e0983705da79f
vicenteas1 <vicenteas.estudios8059@gmail.com>
2025-08-25 05:15:03 -0400

    Se agrega error boundary a main tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import AppRoutes from "./routes/routes.component";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./main.scss";
+import ErrorBoundary from "./components/error-boundary/error-boundary.component";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppRoutes />
+    <ErrorBoundary>
+      <AppRoutes />
+    </ErrorBoundary>
   </StrictMode>
 );
